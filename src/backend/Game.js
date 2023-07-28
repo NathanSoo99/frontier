4f91e4bec4ce207746a51dbd02dad55cc8c5aee7
Nathan <nathansoo99@gmail.com>
2023-07-28 15:37:03 +1000

    added basic resources to the backend

--- src/backend/Game.js
+++ src/backend/Game.js
@@ -1,16 +1,34 @@
-import { buildingData } from './buildings/BuildingReference'
+import { buildingData, buildingStats } from './buildings/BuildingReference'
 import Person from './Person'
 
 const Game = class Game {
     constructor() {
         this.buildings = []
         this.people = []
+        this.resources = {
+            food: 1000,
+            stone: 1000,
+            metal: 1000,
+            wood: 1000
+        }
     }
 
     addBuilding = (key) => {
-        const building = new buildingData[key]()
-        this.buildings.push(building)
-        return building
+        if (buildingStats[key]["cost"].keys.filter(
+            (resource) => (
+                buildingStats["cost"][resource] > this.resources[resource]
+            )
+        ) === []) {
+            const building = new buildingData[key]()
+            this.buildings.push(building)
+            buildingStats[key]["cost"].keys.forEach(
+                (resource) => (
+                    this.resources[resource] -= buildingStats[key]["cost"][resource]
+                )
+            )
+            return building
+        }
+        return null
     }
 
     getBuildings = () => {
@@ -33,6 +51,14 @@ const Game = class Game {
         return person
     }
 
+    setResource = (key, amount) => {
+        this.resources[key] = amount
+        return this.resources[key]
+    }
+
+    getResource = (key) => {
+        return this.resources[key]
+    }
 }
 
 export default Game
--- src/backend/buildings/BuildingReference.js
+++ src/backend/buildings/BuildingReference.js
@@ -12,15 +12,32 @@ export const buildingData = {
 
 export const buildingStats = {
     encampment: {
-        positions: 1
+        positions: 1,
+        cost: {
+            wood: 100,
+            metal: 200,
+            stone: 100
+        }
     },
     mine: {
-        positions: 2
+        positions: 2,
+        cost: {
+            wood: 300
+        }
     },
     quarry: {
-        positions: 3
+        positions: 3,
+        cost: {
+            wood: 200,
+            metal: 100            
+        }
     },
     farm: {
-        positions: 4
+        positions: 4,
+        cost: {
+            wood: 100,
+            metal: 100,
+            stone: 100
+        }
     }
 }
